49254566cacb15358f2e896531f9f68cbca24fa4
Leo Wong <leoelicos@gmail.com>
2022-07-18 23:34:49 +1000

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv');
 console.log('the uri is ', process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks');
 
 module.exports = mongoose.connection;
