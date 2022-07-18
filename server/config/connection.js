8074191161924755bc8d10366c17ae5f335968e1
Leo Wong <leoelicos@gmail.com>
2022-07-18 23:53:14 +1000

    changed mongoose configuration

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-require('dotenv');
-console.log('the uri is ', process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 module.exports = mongoose.connection;
