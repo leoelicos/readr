88e8df83f88c716cbb494378486d605ba38adc45
Leo Wong <leoelicos@gmail.com>
2022-07-18 17:46:35 +1000

    Define front-end GraphQL mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,69 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        password
+      }
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        password
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String], $description: String, $bookId: String, $image: String, $link: String, $title: String) {
+    saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+      _id
+      username
+      email
+      password
+      savedBooks {
+        description
+        bookId
+        image
+        link
+        title
+        authors
+        _id
+      }
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation DeleteBook($bookId: String!) {
+    deleteBook(bookId: $bookId) {
+      _id
+      username
+      email
+      password
+      savedBooks {
+        _id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
