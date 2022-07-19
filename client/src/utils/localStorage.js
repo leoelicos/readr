ed4ea3b49498ec925e6f4623d315cf7e3ca74339
Leo Wong <leoelicos@gmail.com>
2022-07-19 17:28:16 +1000

    Adjust spacing

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,27 +1,28 @@
 export const getSavedBookIds = () => {
   const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
+    ? //
+      JSON.parse(localStorage.getItem('saved_books'))
     : [];
 
   return savedBookIds;
 };
 
 export const saveBookIds = (bookIdArr) => {
-  if (bookIdArr.length) {
-    localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
-  } else {
+  if (!bookIdArr.length) {
     localStorage.removeItem('saved_books');
+    return;
   }
+  localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
 };
 
 export const removeBookId = (bookId) => {
   const savedBookIds = localStorage.getItem('saved_books')
-    ? JSON.parse(localStorage.getItem('saved_books'))
-    : null;
+    ? //
+      JSON.parse(localStorage.getItem('saved_books'))
+    : //
+      null;
 
-  if (!savedBookIds) {
-    return false;
-  }
+  if (!savedBookIds) return false;
 
   const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
   localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
