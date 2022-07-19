18d9b475829df8dc9edd360dd3a8c6b8a9f0544a
Leo Wong <leoelicos@gmail.com>
2022-07-19 13:29:15 +1000

    Improve syntax using newlines

--- server/models/User.js
+++ server/models/User.js
@@ -6,9 +6,21 @@ const bookSchema = require('./Book');
 
 const userSchema = new Schema(
   {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true, match: [/.+@.+\..+/, 'Must use a valid email address'] },
-    password: { type: String, required: true },
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address']
+    },
+    password: {
+      type: String,
+      required: true
+    },
     // set savedBooks to be an array of data that adheres to the bookSchema
     savedBooks: [bookSchema]
   },
