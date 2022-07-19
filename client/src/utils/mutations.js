240f5b9a216113da2d2d036c976e2e16748d130a
Leo Wong <leoelicos@gmail.com>
2022-07-19 13:28:37 +1000

    Use input type to simplify code

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -29,8 +29,8 @@ export const LOGIN = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($authors: [String], $description: String, $bookId: String, $image: String, $link: String, $title: String) {
-    saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
       _id
       username
       email
