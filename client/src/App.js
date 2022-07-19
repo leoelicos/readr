edfbf49752da10451e58384ce0776ea55d20f011
Leo Wong <leoelicos@gmail.com>
2022-07-19 17:29:36 +1000

    Adjust spacing

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const httpLink = createHttpLink({
-  uri: '/graphql'
-});
+const httpLink = createHttpLink({ uri: '/graphql' });
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : ''
+      authorization: token
+        ? //
+          `Bearer ${token}`
+        : ''
     }
   };
 });
@@ -34,7 +34,9 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<SearchBooks />} />
+
             <Route path="/saved" element={<SavedBooks />} />
+
             <Route path="*" element={<h1 className="display-2">Wrong page!</h1>} />
           </Routes>
         </>
