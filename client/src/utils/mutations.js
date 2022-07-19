4a8c1f266250dc31985228562e6f89053dec69f5
Leo Wong <leoelicos@gmail.com>
2022-07-19 11:33:44 +1000

    Change CREATE_USER to ADD_USER

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_USER = gql`
+export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
