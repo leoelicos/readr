8e38e4dcdd6270dad6a4c2987eba8e19c5375c15
Leo Wong <leoelicos@gmail.com>
2022-07-19 22:57:16 +1000

    Comment header and comments

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,3 +1,11 @@
+/* 
+
+Book Search Engine
+SignupForm.js
+SignupForm is a React component that allows a new user to sign up
+
+*/
+
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
@@ -5,17 +13,25 @@ import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  /* user form data */
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+
+  /* validated */
   const [validated] = useState(false);
+
+  /* show alert */
   const [showAlert, setShowAlert] = useState(false);
 
+  /* add user */
   const [addUser] = useMutation(ADD_USER);
 
+  /* user form data */
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  /* add user, show alert, user form data */
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -45,28 +61,35 @@ const SignupForm = () => {
 
   return (
     <>
+      {/* form */}
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {/* alert */}
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant="danger">
           Something went wrong with your signup!
         </Alert>
 
+        {/* username */}
         <Form.Group>
           <Form.Label htmlFor="username">Username</Form.Label>
           <Form.Control type="text" placeholder="Your username" name="username" onChange={handleInputChange} value={userFormData.username} required />
           <Form.Control.Feedback type="invalid">Username is required!</Form.Control.Feedback>
         </Form.Group>
 
+        {/* email */}
         <Form.Group>
           <Form.Label htmlFor="email">Email</Form.Label>
           <Form.Control type="email" placeholder="Your email address" name="email" onChange={handleInputChange} value={userFormData.email} required />
           <Form.Control.Feedback type="invalid">Email is required!</Form.Control.Feedback>
         </Form.Group>
 
+        {/* password */}
         <Form.Group>
           <Form.Label htmlFor="password">Password</Form.Label>
           <Form.Control type="password" placeholder="Your password" name="password" onChange={handleInputChange} value={userFormData.password} required />
           <Form.Control.Feedback type="invalid">Password is required!</Form.Control.Feedback>
         </Form.Group>
+
+        {/* submit button */}
         <Button disabled={!(userFormData.username && userFormData.email && userFormData.password)} type="submit" variant="success">
           Submit
         </Button>
